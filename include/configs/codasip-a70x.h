2f5483ce94f124cd8e7bec4f3b2bdec5c6536206
Stuart Menefy <stuart.menefy@codasip.com>
2024-01-31 17:39:58 +0000

    board: codasip: Switch to distro_bootcmd
    To make it easier for distributions to configure the boot
    environment, switch to the standard distro_bootcmd method.
    
    Signed-off-by: Stuart Menefy <stuart.menefy@codasip.com>

--- include/configs/codasip-a70x.h
+++ include/configs/codasip-a70x.h
@@ -3,9 +3,16 @@
 
 #include <linux/sizes.h>
 
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0)
+
+#include <config_distro_bootcmd.h>
+
 #define CFG_EXTRA_ENV_SETTINGS \
+	"scriptaddr=0x88000000\0" \
 	"fdt_high=0xffffffffffffffff\0" \
 	"kernel_comp_addr_r=0x84000000\0" \
 	"kernel_comp_size=0x4000000\0" \
+	BOOTENV
 
 #endif /* CODASIP_A70X_H */
