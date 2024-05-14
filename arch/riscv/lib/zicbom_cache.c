ad4c0bd5db6bd5464dede40d929bd48729f12c2e
David McKay <david.mckay@codasip.com>
2024-05-14 12:49:37 +0100

    riscv: Add zicbom extension support
    The Zicbom extension adds in cache flushing instructions into the riscv
    instruction set. This patch adds in support for it, primarily for
    non-coherent DMA. If you enable this option, you will have to have a
    compiler that understands the mnemonic.
    
    Signed-off-by: David McKay <david.mckay@codasip.com>
    [Stuart: Updated asm constraints for CHERI]
    Signed-off-by: Stuart Menefy <stuart.menefy@codasip.com>

---
+++ arch/riscv/lib/zicbom_cache.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ */
+
+#include <asm/asm.h>
+#include <asm/cheri.h>
+#include <cpu_func.h>
+
+#ifndef CONFIG_SYS_CACHELINE_SIZE
+#error "CONFIG_SYS_CACHELINE_SIZE must be defined"
+#endif
+
+void flush_dcache_range(unsigned long start, unsigned long end)
+{
+	void *addr, *end_addr;
+
+	start &= ~(CONFIG_SYS_CACHELINE_SIZE - 1);
+
+#ifdef CONFIG_RISCV_ISA_ZCHERIPURECAP_ABI
+	addr = cheri_build_infinite_cap(start);
+	end_addr = cheri_build_infinite_cap(end);
+#else
+	addr = (void*)start;
+	end_addr = (void*)end;
+#endif
+
+	for ( ; addr < end; addr += CONFIG_SYS_CACHELINE_SIZE)
+		 __asm__ __volatile__ ("cbo.flush 0(%[addr])\n" :: [addr] PTR_REG(addr));
+}
+
+void invalidate_dcache_range(unsigned long start, unsigned long end)
+{
+	flush_dcache_range(start, end);
+}
+
