58dffbd2c4b006066ecb539bc0b9eeb53d677680
David McKay <david.mckay@codasip.com>
2024-07-25 11:38:25 +0100

    hobgoblin: Add proFPGA board support
    The proFPGA board has to use a different memory layout to the other
    boards we support due to limitations in the Vivido interconnect
    generator, namely that it requires memory to be aligned on a natural
    boundary. This means that 16Gb of memory cannot be placed at the usual
    0x80000000 address.
    
    This means we have to build uboot for the proFPGA board at a different
    address. We do not have the same problem for openSBI since later version
    are relocatable.
    
    Signed-off-by: David McKay <david.mckay@codasip.com>

--- include/configs/codasip-a730-hobgoblin.h
+++ include/configs/codasip-a730-hobgoblin.h
@@ -8,11 +8,22 @@
 
 #include <config_distro_bootcmd.h>
 
+#if defined(CONFIG_TARGET_CODASIP_A730_HOBGOBLIN_PROFPGA)
+#define CFG_EXTRA_ENV_SETTINGS \
+	"scriptaddr=0x2008000000\0" \
+	"fdt_high=0xffffffffffffffff\0" \
+	"kernel_comp_addr_r=0x2004000000\0" \
+	"kernel_comp_size=0x4000000\0" \
+	BOOTENV
+#elif defined(CONFIG_TARGET_CODASIP_A730_HOBGOBLIN)
 #define CFG_EXTRA_ENV_SETTINGS \
 	"scriptaddr=0x88000000\0" \
 	"fdt_high=0xffffffffffffffff\0" \
 	"kernel_comp_addr_r=0x84000000\0" \
 	"kernel_comp_size=0x4000000\0" \
 	BOOTENV
+#else
+#error "No codasip target defined!!"
+#endif
 
 #endif /* CCODASIP_A730_HOBGOBLIN_H */
