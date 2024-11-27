e5700a014e752653fedc46fa34fe848282193231
Ka Leung Ho <ka.ho@codasip.com>
2024-11-27 17:02:11 +0000

    malloc_simple: Aligned the malloc_ptr to uintptr_t
    Make sure the memory malloc is aligned to uintptr_t rather
    than ulong since the return memory may be used to store
    a pointer which may not be the same as native word (ulong)
    on some architectures (e.g. CHERI).

--- common/malloc_simple.c
+++ common/malloc_simple.c
@@ -31,7 +31,7 @@ static void *alloc_simple(size_t bytes, int align)
 	}
 
 	ptr = map_sysmem(addr, bytes);
-	gd->malloc_ptr = ALIGN(new_ptr, sizeof(new_ptr));
+	gd->malloc_ptr = ALIGN(new_ptr, sizeof(uintptr_t));
 
 	return ptr;
 }
@@ -40,7 +40,7 @@ void *malloc_simple(size_t bytes)
 {
 	void *ptr;
 
-	ptr = alloc_simple(bytes, 1);
+	ptr = alloc_simple(bytes, sizeof(uintptr_t));
 	if (!ptr)
 		return ptr;
 
