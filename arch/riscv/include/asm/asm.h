5f7e897595fac01bbea3019809a2fe910dffe2ef
Ka Leung Ho <ka.ho@codasip.com>
2024-11-27 13:21:04 +0000

    riscv: cheri: Add Zcheri supports to asm.h
    Defines macro to reprensent different usage for GPRs and CSR to improve the
    code portability for Zcheri extension.
    - REG_NUM(r) represents native GPRs with number index. e.g. REG_NUM(0) is
      x0 on non-CHERI and is c0 on CHERI.
    - REG(r) represents native GPRs with ABI name. e.g. REG(a0) is a0 on
      non-CHERI and ca0 on CHERI.
    - PREG(r) represents native GPRs for storing pointer with ABI name. e.g.
      PREG(sp) is sp on non-CHERI and csp on CHERI.
    - PCSR(csr) represents native CSRs for storing pointer with CSR name.
      e.g. PCSR(mepc) is mepc on non-CHERI and mepcc on CHERI.
    
    Add REG, XREG aand PREG load/store macro to improve the code portability
    for CHERI extension. The native registers on the core with CHERI
    extension is capability register.
    - REG is the native register. REG is XREG on non-CHERI and CREG on CHERI.
    - XREG is the register for storing native word. XREG is same on both of
      non-CHERI and CHERI.
    - PREG is the register for storing native pointer. PREG is XREG on
      non-CHERI and CREG on CHERI.
    
    PC_PTR_L and PTR_L is added. For non-CHERI both are macro of la
    pseudo-instruction. For CHERI, PC_PTR_L is llc which derive the offset
    pointer with the pcc as the result, the provenance of derived pointer is
    same as pcc. It can be used before and after capability being initialized.
    PTR_L can be used only after the capability initialization. The capability
    pointer provenance is set during capability initialization.

--- arch/riscv/include/asm/asm.h
+++ arch/riscv/include/asm/asm.h
@@ -12,6 +12,53 @@
 #define __ASM_STR(x)	#x
 #endif
 
+#ifdef CONFIG_RISCV_ISA_ZCHERIPURECAP_ABI
+#ifdef __ASSEMBLY__
+# define REG_NUM(r)			c ## r
+
+# define REG_ZERO			cnull
+# define REG(r)				c ## r
+
+# define PREG_NULL			cnull
+# define PREG(r)			c ## r
+
+# define __PCSR(r)			r ## c
+# define PCSR(r)			__PCSR(r)
+#else
+# define REG_NUM(r)			"c" #r
+
+# define REG_ZERO			"cnull"
+# define REG(r)				"c" #r
+
+# define PREG_NULL			"cnull"
+# define PREG(r)			"c" #r
+
+# define PCSR(r)			#r "c"
+#endif
+#else /* !CONFIG_RISCV_ISA_ZCHERIPURECAP_ABI */
+#ifdef __ASSEMBLY__
+# define REG_NUM(r)			x ## r
+
+# define REG_ZERO			zero
+# define REG(r)				r
+
+# define PREG_NULL			zero
+# define PREG(r)			r
+
+# define PCSR(r)			r
+#else
+# define REG_NUM(r)			"x" #r
+
+# define REG_ZERO			"zero"
+# define REG(r)				#r
+
+# define PREG_NULL			"zero"
+# define PREG(r)			 #r
+
+# define PCSR(r)			#r
+#endif
+#endif /* !CONFIG_RISCV_ISA_ZCHERIPURECAP_ABI */
+
 #if __riscv_xlen == 64
 #define __REG_SEL(a, b)	__ASM_STR(a)
 #elif __riscv_xlen == 32
@@ -20,10 +67,70 @@
 #error "Unexpected __riscv_xlen"
 #endif
 
+#ifdef CONFIG_RISCV_ISA_ZCHERIPURECAP_ABI
+#define REG_L		__REG_SEL(lc, lc)
+#define REG_S		__REG_SEL(sc, sc)
+#define SZREG		__REG_SEL(16, 8)
+#define LGREG		__REG_SEL(4, 3)
+
+#define XREG_L		__REG_SEL(ld, lw)
+#define XREG_S		__REG_SEL(sd, sw)
+#define SZXREG		__REG_SEL(8, 4)
+#define LGXREG		__REG_SEL(3, 2)
+
+#define PREG_L		__REG_SEL(lc, lc)
+#define PREG_S		__REG_SEL(sc, sc)
+#define SZPREG		__REG_SEL(16, 8)
+#define LGPREG		__REG_SEL(4, 3)
+
+#define PC_PTR_L	__ASM_STR(llc)
+#define PTR_L		__ASM_STR(lgc)
+
+#define PTR_REG	    "C"
+
+#if __SIZEOF_POINTER__ == 16
+#ifdef __ASSEMBLY__
+#define RISCV_PTR		.chericap
+#define RISCV_SZPTR		16
+#define RISCV_LGPTR		4
+#else
+#define RISCV_PTR		".chericap"
+#define RISCV_SZPTR		"16"
+#define RISCV_LGPTR		"4"
+#endif
+#elif __SIZEOF_POINTER__ == 8
+#ifdef __ASSEMBLY__
+#define RISCV_PTR		.chericap
+#define RISCV_SZPTR		8
+#define RISCV_LGPTR		3
+#else
+#define RISCV_PTR		".chericap"
+#define RISCV_SZPTR		"8"
+#define RISCV_LGPTR		"3"
+#endif
+#else
+#error "Unexpected __SIZEOF_POINTER__"
+#endif
+#else /* !CONFIG_RISCV_ISA_ZCHERIPURECAP_ABI */
 #define REG_L		__REG_SEL(ld, lw)
 #define REG_S		__REG_SEL(sd, sw)
 #define SZREG		__REG_SEL(8, 4)
 #define LGREG		__REG_SEL(3, 2)
+
+#define XREG_L		__REG_SEL(ld, lw)
+#define XREG_S		__REG_SEL(sd, sw)
+#define SZXREG		__REG_SEL(8, 4)
+#define LGXREG		__REG_SEL(3, 2)
+
+#define PREG_L		__REG_SEL(ld, lw)
+#define PREG_S		__REG_SEL(sd, sw)
+#define SZPREG		__REG_SEL(8, 4)
+#define LGPREG		__REG_SEL(3, 2)
+
+#define PC_PTR_L	__ASM_STR(la)
+#define PTR_L		__ASM_STR(la)
+
+#define PTR_REG	    "r"
 
 #if __SIZEOF_POINTER__ == 8
 #ifdef __ASSEMBLY__
@@ -48,6 +155,7 @@
 #else
 #error "Unexpected __SIZEOF_POINTER__"
 #endif
+#endif /* !CONFIG_RISCV_ISA_ZCHERIPURECAP_ABI */
 
 #if (__SIZEOF_INT__ == 4)
 #define RISCV_INT		__ASM_STR(.word)
