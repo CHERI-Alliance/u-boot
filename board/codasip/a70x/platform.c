42021465d154fd066432019a2768e0ffa06bb516
Ka Leung Ho <ka.ho@codasip.com>
2024-11-27 08:54:44 +0000

    board: codasip: Add cheri support to a70x platform
    Return a capability pointer for the FDT blob if u-boot is built for
    cheri purecap mode.

--- board/codasip/a70x/platform.c
+++ board/codasip/a70x/platform.c
@@ -3,6 +3,7 @@
 #include <cpu_func.h>
 #include <dm.h>
 #include <asm/sections.h>
+#include <asm/io.h>
 
 int misc_init_r(void)
 {
@@ -14,10 +15,10 @@ void *board_fdt_blob_setup(int *err)
 	*err = 0;
 	if (IS_ENABLED(CONFIG_OF_SEPARATE) || IS_ENABLED(CONFIG_OF_BOARD)) {
 		if (gd->arch.firmware_fdt_addr)
-			return (ulong *)(uintptr_t)gd->arch.firmware_fdt_addr;
+			return map_physmem(gd->arch.firmware_fdt_addr, 0, MAP_DATA);
 	}
 
-	return (ulong *)&_end;
+	return map_physmem((ulong)&_end, 0, MAP_DATA);
 }
 
 int board_init(void)
