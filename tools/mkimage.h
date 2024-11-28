a4d4a22168207c1b3a86c82a6ca216b8912089f2
Ka Leung Ho <ka.ho@codasip.com>
2024-11-28 09:23:18 +0000

    mkimage: Add map_physmem() and relative definitions
    Since the host tool share the image code (whichis common with mkimage).
    To avoid the compilation error,  define dummy version of these here.
    Same as adding map_sysmem() in commit 2434c66da041 ("mkimage: Add
    map_sysmem() and IH_ARCH_DEFAULT to simplfy building").

--- tools/mkimage.h
+++ tools/mkimage.h
@@ -32,6 +32,8 @@
 
 #define log_debug(fmt, args...)	debug(fmt, ##args)
 
+typedef unsigned long phys_addr_t;
+
 static inline void *map_sysmem(ulong paddr, unsigned long len)
 {
 	return (void *)(uintptr_t)paddr;
@@ -40,6 +42,25 @@ static inline void *map_sysmem(ulong paddr, unsigned long len)
 static inline ulong map_to_sysmem(void *ptr)
 {
 	return (ulong)(uintptr_t)ptr;
+}
+
+# define MAP_NOCACHE	0	/* Produce an uncached mapping */
+# define MAP_WRCOMBINE	0	/* Allow write-combining on the mapping */
+# define MAP_WRBACK	0	/* Map using write-back caching */
+# define MAP_WRTHROUGH	0	/* Map using write-through caching */
+# define MAP_IO		0	/* Map for memory mapped IO */
+# define MAP_DATA	0	/* Map for data */
+# define MAP_RO_DATA	0	/* Map for read only data */
+# define MAP_EXE	0	/* Map for executable */
+
+static inline void *map_physmem(phys_addr_t paddr, unsigned long len,
+				unsigned long flags)
+{
+	return (void *)(uintptr_t)paddr;
+}
+
+static inline void unmap_physmem(void *vaddr, unsigned long flags)
+{
 }
 
 #define ALLOC_CACHE_ALIGN_BUFFER(type, name, size) type name[size]
