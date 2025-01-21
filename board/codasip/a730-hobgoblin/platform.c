997892a1f2b4312bf11a03d882167b129b75010a
Stuart Menefy <stuart.menefy@codasip.com>
2025-01-21 14:52:37 +0000

    board: codasip: hobgoblin: Support reserved-memory in device tree
    For systems using the CMU to manage CHERI tags there are likely to be
    reserved memory regions at the top of memory, which conflict with the
    address U-Boot will pick for the relocated code etc. So instead use
    the LMB (Logical memory blocks) code, initialised from the device
    tree, to pick a region of memory for U-Boot's relocated data regions.
    
    This is effectively a copy of the code from
    arch/arm/mach-stm32mp/dram_init.c and board/xilinx/common/board.c.
    
    Signed-off-by: Stuart Menefy <stuart.menefy@codasip.com>

--- board/codasip/a730-hobgoblin/platform.c
+++ board/codasip/a730-hobgoblin/platform.c
@@ -2,6 +2,8 @@
 
 #include <cpu_func.h>
 #include <dm.h>
+#include <lmb.h>
+#include <image.h>
 #include <asm/sections.h>
 #include <asm/io.h>
 
@@ -25,3 +27,34 @@ int board_init(void)
 {
 	return 0;
 }
+
+
+#if defined(CONFIG_LMB)
+
+#ifndef MMU_SECTION_SIZE
+#define MMU_SECTION_SIZE        (1 * 1024 * 1024)
+#endif
+
+/* Get the top of usable RAM */
+phys_addr_t board_get_usable_ram_top(phys_size_t total_size)
+{
+	phys_size_t size;
+	phys_addr_t reg;
+	struct lmb lmb;
+
+	if (!total_size)
+		return gd->ram_top;
+
+	/* Find enough non-reserved memory to relocate U-Boot */
+	lmb_init(&lmb);
+	lmb_add(&lmb, gd->ram_base, gd->ram_size);
+	boot_fdt_add_mem_rsv_regions(&lmb, (void *)gd->fdt_blob);
+	size = ALIGN(CONFIG_SYS_MALLOC_LEN + total_size, MMU_SECTION_SIZE);
+	reg = lmb_alloc(&lmb, size, MMU_SECTION_SIZE);
+
+	if (!reg)
+		reg = gd->ram_top - size;
+
+	return reg + size;
+}
+#endif
