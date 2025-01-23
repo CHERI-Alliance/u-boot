CHERI-Alliance/u-boot

commit d1087506c923423008a870ad721c2a9e9399adfa

driver: Fix pointer casting
Change pointer casting to preseve the pointer and avoid compiling warning
on the architectures like CHERI where the pointer is not an unsigned long
integer.

- Use uintptr_t type for preseving original pointer.
- Use unsigned long type for extracting address from pointer.
- Use ioremap() to get io accessing pointer from physical address.
- Use dev_read_addr_index_ptr() rather than dev_read_addr_index() to get
  pointer points to IO from dev subsystem.

drivers/net/xilinx_axi_emac.c
@@ -903,9 +903,10 @@ static int axi_emac_of_to_plat(struct udevice *dev)
 	ret = dev_read_phandle_with_args(dev, "axistream-connected", NULL, 0, 0,
 					 &axistream_node);
 	if (!ret)
-		plat->dmatx = (struct axidma_reg *)ofnode_get_addr(axistream_node.node);
+		plat->dmatx = (struct axidma_reg *)ioremap(ofnode_get_addr(axistream_node.node),
+							   sizeof(struct axidma_reg));
 	else
-		plat->dmatx = (struct axidma_reg *)dev_read_addr_index(dev, 1);
+		plat->dmatx = (struct axidma_reg *)dev_read_addr_index_ptr(dev, 1);
 
 	if (!plat->dmatx) {
 		printf("%s: axi_dma register space not found\n", __func__);

drivers/net/zynq_gem.c
@@ -852,7 +852,7 @@ static int zynq_gem_probe(struct udevice *dev)
 
 	/* Initialize the bd spaces for tx and rx bd's */
 	priv->tx_bd = (struct emac_bd *)bd_space;
-	priv->rx_bd = (struct emac_bd *)((ulong)bd_space + BD_SEPRN_SPACE);
+	priv->rx_bd = (struct emac_bd *)((uintptr_t)bd_space + BD_SEPRN_SPACE);
 
 	ret = clk_get_by_name(dev, "tx_clk", &priv->tx_clk);
 	if (ret < 0) {
@@ -956,7 +956,7 @@ static int zynq_gem_of_to_plat(struct udevice *dev)
 	struct ofnode_phandle_args phandle_args;
 
 	pdata->iobase = (phys_addr_t)dev_read_addr(dev);
-	priv->iobase = (struct zynq_gem_regs *)pdata->iobase;
+	priv->iobase = (struct zynq_gem_regs *)ioremap(pdata->iobase, sizeof(struct zynq_gem_regs));
 	priv->mdiobase = priv->iobase;
 	/* Hardcode for now */
 	priv->phyaddr = -1;
@@ -983,7 +983,9 @@ static int zynq_gem_of_to_plat(struct udevice *dev)
 		addr = ofnode_get_addr(parent);
 		if (addr != FDT_ADDR_T_NONE) {
 			debug("MDIO bus not found %s\n", dev->name);
-			priv->mdiobase = (struct zynq_gem_regs *)addr;
+			priv->mdiobase =
+				(struct zynq_gem_regs *)ioremap(addr,
+								sizeof(struct zynq_gem_regs));
 		}
 	}
 

drivers/ram/aspeed/sdram_ast2600.c
@@ -1136,9 +1136,9 @@ static int ast2600_sdrammc_of_to_plat(struct udevice *dev)
 {
 	struct dram_info *priv = dev_get_priv(dev);
 
-	priv->regs = (void *)(uintptr_t)devfdt_get_addr_index(dev, 0);
-	priv->phy_setting = (void *)(uintptr_t)devfdt_get_addr_index(dev, 1);
-	priv->phy_status = (void *)(uintptr_t)devfdt_get_addr_index(dev, 2);
+	priv->regs = devfdt_get_addr_index_ptr(dev, 0));
+	priv->phy_setting = devfdt_get_addr_index_ptr(dev, 1));
+	priv->phy_status = devfdt_get_addr_index_ptr(dev, 2));
 
 	priv->clock_rate = fdtdec_get_int(gd->fdt_blob, dev_of_offset(dev),
 					  "clock-frequency", 0);

drivers/scsi/scsi.c
@@ -135,7 +135,7 @@ static ulong scsi_read(struct udevice *dev, lbaint_t blknr, lbaint_t blkcnt,
 	/* Setup device */
 	pccb->target = block_dev->target;
 	pccb->lun = block_dev->lun;
-	buf_addr = (unsigned long)buffer;
+	buf_addr = (uintptr_t)buffer;
 	start = blknr;
 	blks = blkcnt;
 	if (uc_plat->max_bytes_per_req)
@@ -174,7 +174,7 @@ static ulong scsi_read(struct udevice *dev, lbaint_t blknr, lbaint_t blkcnt,
 		}
 		debug("scsi_read_ext: startblk " LBAF
 		      ", blccnt %x buffer %lX\n",
-		      start, smallblks, buf_addr);
+		      start, smallblks, (unsigned long)buf_addr);
 		if (scsi_exec(bdev, pccb)) {
 			scsi_print_error(pccb);
 			blkcnt -= blks;
@@ -183,7 +183,7 @@ static ulong scsi_read(struct udevice *dev, lbaint_t blknr, lbaint_t blkcnt,
 		buf_addr += pccb->datalen;
 	} while (blks != 0);
 	debug("scsi_read_ext: end startblk " LBAF
-	      ", blccnt %x buffer %lX\n", start, smallblks, buf_addr);
+	      ", blccnt %x buffer %lX\n", start, smallblks, (unsigned long)buf_addr);
 	return blkcnt;
 }
 
@@ -205,7 +205,7 @@ static ulong scsi_write(struct udevice *dev, lbaint_t blknr, lbaint_t blkcnt,
 	/* Setup device */
 	pccb->target = block_dev->target;
 	pccb->lun = block_dev->lun;
-	buf_addr = (unsigned long)buffer;
+	buf_addr = (uintptr_t)buffer;
 	start = blknr;
 	blks = blkcnt;
 	if (uc_plat->max_bytes_per_req)
@@ -232,7 +232,7 @@ static ulong scsi_write(struct udevice *dev, lbaint_t blknr, lbaint_t blkcnt,
 			blks = 0;
 		}
 		debug("%s: startblk " LBAF ", blccnt %x buffer %lx\n",
-		      __func__, start, smallblks, buf_addr);
+		      __func__, start, smallblks, (unsigned long)buf_addr);
 		if (scsi_exec(bdev, pccb)) {
 			scsi_print_error(pccb);
 			blkcnt -= blks;
@@ -241,7 +241,7 @@ static ulong scsi_write(struct udevice *dev, lbaint_t blknr, lbaint_t blkcnt,
 		buf_addr += pccb->datalen;
 	} while (blks != 0);
 	debug("%s: end startblk " LBAF ", blccnt %x buffer %lX\n",
-	      __func__, start, smallblks, buf_addr);
+	      __func__, start, smallblks, (unsigned long)buf_addr);
 	return blkcnt;
 }
 

drivers/serial/serial_htif.c
@@ -13,6 +13,7 @@
 #include <linux/compiler.h>
 #include <serial.h>
 #include <linux/err.h>
+#include <linux/io.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -143,12 +144,12 @@ static int htif_serial_of_to_plat(struct udevice *dev)
 	addr = dev_read_addr_index(dev, 0);
 	if (addr == FDT_ADDR_T_NONE)
 		return -ENODEV;
-	plat->fromhost = (void *)(uintptr_t)addr;
+	plat->fromhost = (void *)ioremap(addr, sizeof(u64) * 2);
 	plat->tohost = plat->fromhost + sizeof(u64);
 
 	addr = dev_read_addr_index(dev, 1);
 	if (addr != FDT_ADDR_T_NONE)
-		plat->tohost = (void *)(uintptr_t)addr;
+		plat->tohost = (void *)ioremap(addr, sizeof(u64));
 
 	plat->console_char = -1;
 

drivers/serial/serial_sifive.c
@@ -178,9 +178,9 @@ static int sifive_serial_of_to_plat(struct udevice *dev)
 {
 	struct sifive_uart_plat *plat = dev_get_plat(dev);
 
-	plat->regs = (struct uart_sifive *)(uintptr_t)dev_read_addr(dev);
-	if (IS_ERR(plat->regs))
-		return PTR_ERR(plat->regs);
+	plat->regs = (struct uart_sifive *)dev_read_addr_ptr(dev);
+	if (!plat->regs)
+		return -ENXIO;
 
 	return 0;
 }
@@ -211,7 +211,8 @@ U_BOOT_DRIVER(serial_sifive) = {
 static inline void _debug_uart_init(void)
 {
 	struct uart_sifive *regs =
-			(struct uart_sifive *)CONFIG_VAL(DEBUG_UART_BASE);
+		(struct uart_sifive *)ioremap(CONFIG_VAL(DEBUG_UART_BASE),
+					      sizeof(struct uart_sifive));
 
 	_sifive_serial_setbrg(regs, CONFIG_DEBUG_UART_CLOCK,
 			      CONFIG_BAUDRATE);
@@ -221,7 +222,8 @@ static inline void _debug_uart_init(void)
 static inline void _debug_uart_putc(int ch)
 {
 	struct uart_sifive *regs =
-			(struct uart_sifive *)CONFIG_VAL(DEBUG_UART_BASE);
+		(struct uart_sifive *)ioremap(CONFIG_VAL(DEBUG_UART_BASE),
+					      sizeof(struct uart_sifive));
 
 	while (_sifive_serial_putc(regs, ch) == -EAGAIN)
 		schedule();

drivers/timer/riscv_aclint_timer.c
@@ -17,7 +17,7 @@
 #define ACLINT_MTIME_OFFSET		0
 
 /* mtime register */
-#define MTIME_REG(base, offset)		((ulong)(base) + (offset))
+#define MTIME_REG(base, offset)		((uintptr_t)(base) + (offset))
 
 static u64 notrace riscv_aclint_timer_get_count(struct udevice *dev)
 {
@@ -40,8 +40,9 @@ unsigned long notrace timer_early_get_rate(void)
  */
 u64 notrace timer_early_get_count(void)
 {
-	return readq((void __iomem *)MTIME_REG(RISCV_MMODE_TIMERBASE,
-					       RISCV_MMODE_TIMEROFF));
+	return readq((void __iomem *)ioremap(MTIME_REG(RISCV_MMODE_TIMERBASE,
+						       RISCV_MMODE_TIMEROFF),
+					     sizeof(u64)));
 }
 #endif
 
@@ -58,8 +59,7 @@ ulong timer_get_boot_us(void)
 		timer_get_count(gd->timer, &ticks);
 	} else {
 		rate = RISCV_MMODE_TIMER_FREQ;
-		ticks = readq((void __iomem *)MTIME_REG(RISCV_MMODE_TIMERBASE,
-							RISCV_MMODE_TIMEROFF));
+		ticks = timer_early_get_count();
 	}
 
 	/* Below is converted from time(us) = (tick / rate) * 10000000 */

drivers/ufs/ufs.c
@@ -2012,7 +2012,7 @@ static int ufs_scsi_buffer_aligned(struct udevice *scsi_dev, struct bounce_buffe
 	if ((hba->quirks & UFSHCD_QUIRK_BROKEN_64BIT_ADDRESS) &&
 	    ((ubuf >> 32) || (ubuf + len) >> 32)) {
 		dev_dbg(scsi_dev, "Buffer above 32bit boundary %lx-%lx\n",
-			ubuf, ubuf + len);
+			(unsigned long)ubuf, (unsigned long)ubuf + len);
 		return 0;
 	}
 #endif

commit 3956504641db071e122b89d4656d9da3d1e13d79

net: axi_emac: Fix non-CHERI build
Need to switch from asm/io.h to linux/io.h to get a dummy definition
of ioremap() for non-CHERI builds.

Signed-off-by: Stuart Menefy <stuart.menefy@codasip.com>
Fixes: d1087506c923 ("driver: Fix pointer casting")

drivers/net/xilinx_axi_emac.c
@@ -15,7 +15,7 @@
 #include <net.h>
 #include <malloc.h>
 #include <asm/global_data.h>
-#include <asm/io.h>
+#include <linux/io.h>
 #include <phy.h>
 #include <miiphy.h>
 #include <wait_bit.h>