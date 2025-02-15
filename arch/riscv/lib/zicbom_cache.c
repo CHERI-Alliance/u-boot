CHERI-Alliance/u-boot

commit ad4c0bd5db6bd5464dede40d929bd48729f12c2e

riscv: Add zicbom extension support
The Zicbom extension adds in cache flushing instructions into the riscv
instruction set. This patch adds in support for it, primarily for
non-coherent DMA. If you enable this option, you will have to have a
compiler that understands the mnemonic.

Signed-off-by: David McKay <david.mckay@codasip.com>
[Stuart: Updated asm constraints for CHERI]
Signed-off-by: Stuart Menefy <stuart.menefy@codasip.com>

arch/riscv/lib/zicbom_cache.c
@@ -1,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ */
+
+#include <asm/asm.h>
+#include <asm/cheri.h>
+#include <cpu_func.h>
+
+#ifndef CONFIG_SYS_CACHELINE_SIZE
+#error "CONFIG_SYS_CACHELINE_SIZE must be defined"
+#endif
+
+void flush_dcache_range(unsigned long start, unsigned long end)
+{
+	void *addr, *end_addr;
+
+	start &= ~(CONFIG_SYS_CACHELINE_SIZE - 1);
+
+#ifdef CONFIG_RISCV_ISA_ZCHERIPURECAP_ABI
+	addr = cheri_build_infinite_cap(start);
+	end_addr = cheri_build_infinite_cap(end);
+#else
+	addr = (void*)start;
+	end_addr = (void*)end;
+#endif
+
+	for ( ; addr < end; addr += CONFIG_SYS_CACHELINE_SIZE)
+		 __asm__ __volatile__ ("cbo.flush 0(%[addr])\n" :: [addr] PTR_REG(addr));
+}
+
+void invalidate_dcache_range(unsigned long start, unsigned long end)
+{
+	flush_dcache_range(start, end);
+}
+

arch/riscv/Kconfig
@@ -357,6 +357,14 @@ endmenu
 config RISCV_ISA_A
 	def_bool y
 
+config RISCV_ISA_ZICBOM
+	bool "Use Zicbom extension for data cache flushing"
+	default n
+	help
+	  Use the standard RISCV Zicbom extension for flushing the data cache. This
+	  will flush both the L1 and L2 cache if present. Necessary if DMA is not
+	  cache coherent with the CPU. Adds "zicbom"  to the ISA string
+
 config DMA_ADDR_T_64BIT
 	bool
 	default y if 64BIT

arch/riscv/Makefile
@@ -59,6 +59,10 @@ ifeq ($(toolchain-need-zicsr-zifencei),y)
 	RISCV_MARCH := $(RISCV_MARCH)_zicsr_zifencei
 endif
 
+ifeq ($(CONFIG_RISCV_ISA_ZICBOM),y)
+	RISCV_MARCH := $(RISCV_MARCH)_zicbom
+endif
+
 ARCH_FLAGS = -march=$(RISCV_MARCH) -mabi=$(ABI) \
 	     -mcmodel=$(CMODEL)
 

arch/riscv/lib/Makefile
@@ -11,6 +11,7 @@ obj-$(CONFIG_CMD_BOOTI) += bootm.o image.o
 obj-$(CONFIG_CMD_GO) += boot.o
 obj-y	+= cache.o
 obj-$(CONFIG_SIFIVE_CACHE) += sifive_cache.o
+obj-$(CONFIG_RISCV_ISA_ZICBOM) += zicbom_cache.o
 ifeq ($(CONFIG_$(SPL_)RISCV_MMODE),y)
 obj-$(CONFIG_$(SPL_)RISCV_ACLINT) += aclint_ipi.o
 obj-$(CONFIG_ANDES_PLICSW) += andes_plicsw.o

commit 2defbf59db5d61ed05aebac683f1e3431b366684

riscv: Fix warning in zicbom_cache.c
Fix the warning message from zicbom_cache.c:
arch/riscv/lib/zicbom_cache.c:29:15: warning: ordered comparison between pointer and integer ('void *' and 'unsigned long')
   29 |         for ( ; addr < end; addr += CONFIG_SYS_CACHELINE_SIZE)
      |                 ~~~~ ^ ~~~

Signed-off-by: Stuart Menefy <stuart.menefy@codasip.com>
Fixes: ad4c0bd5db6b ("riscv: Add zicbom extension support")

arch/riscv/lib/zicbom_cache.c
@@ -26,7 +26,7 @@ void flush_dcache_range(unsigned long start, unsigned long end)
 	end_addr = (void*)end;
 #endif
 
-	for ( ; addr < end; addr += CONFIG_SYS_CACHELINE_SIZE)
+	for ( ; addr < end_addr; addr += CONFIG_SYS_CACHELINE_SIZE)
 		 __asm__ __volatile__ ("cbo.flush 0(%[addr])\n" :: [addr] PTR_REG(addr));
 }
 