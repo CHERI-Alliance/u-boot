bd8fb3bd197012d90fe53e59070b28eb21232883
Stuart Menefy <stuart.menefy@codasip.com>
2025-07-11 14:17:39 +0100

    configs: hobgoblin: Remove setting fdt_high
    Because we are passing in a FDT at boot time (rather than having one
    embedded in U-Boot or loading it from a file system for example) the
    FDT is relocated at boot time into an allocated buffer, which is sized
    exactly to fit (see common/board_f.c:reserve_fdt).  In this case we
    should not be setting fdt_high to -1, as this prevents
    boot_relocate_fdt() from moving the FDT to a suitable location which
    will be big enough to allow the FDT to be resized.
    
    Signed-off-by: Stuart Menefy <stuart.menefy@codasip.com>

--- include/configs/codasip-a730-hobgoblin.h
+++ include/configs/codasip-a730-hobgoblin.h
@@ -11,14 +11,12 @@
 #if defined(CONFIG_TARGET_CODASIP_A730_HOBGOBLIN_PROFPGA)
 #define CFG_EXTRA_ENV_SETTINGS \
 	"scriptaddr=0x2008000000\0" \
-	"fdt_high=0xffffffffffffffff\0" \
 	"kernel_comp_addr_r=0x2004000000\0" \
 	"kernel_comp_size=0x4000000\0" \
 	BOOTENV
 #elif defined(CONFIG_TARGET_CODASIP_A730_HOBGOBLIN)
 #define CFG_EXTRA_ENV_SETTINGS \
 	"scriptaddr=0x88000000\0" \
-	"fdt_high=0xffffffffffffffff\0" \
 	"kernel_comp_addr_r=0x84000000\0" \
 	"kernel_comp_size=0x4000000\0" \
 	BOOTENV
